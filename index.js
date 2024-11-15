e202e5e2035cd64f11a666f506f21c4e955ffdc1
Rana Huziama Imtiaz <huzaimadigital@gmail.com>
2024-11-15 12:38:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.post('/api/v1/verify-machine', async (req, res) => {
 });
 
 // Start the Server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
